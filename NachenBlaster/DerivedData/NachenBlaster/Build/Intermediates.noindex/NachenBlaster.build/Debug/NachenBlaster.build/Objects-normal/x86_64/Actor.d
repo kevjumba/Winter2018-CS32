64d40d6e7a80908b9328f0a0d7493155ad7e304a
Kevin Zhang <kevin.zhang.13499@gmail.com>
2018-09-15 12:35:59 -0700

    Remove DerivedData

--- NachenBlaster/DerivedData/NachenBlaster/Build/Intermediates.noindex/NachenBlaster.build/Debug/NachenBlaster.build/Objects-normal/x86_64/Actor.d
+++
@@ -1,7 +0,0 @@
-dependencies: \
-  /Users/kevinzhang/Documents/CS/32/NachenBlaster/NachenBlaster/Actor.cpp \
-  /Users/kevinzhang/Documents/CS/32/NachenBlaster/NachenBlaster/Actor.h \
-  /Users/kevinzhang/Documents/CS/32/NachenBlaster/NachenBlaster/GraphObject.h \
-  /Users/kevinzhang/Documents/CS/32/NachenBlaster/NachenBlaster/GameConstants.h \
-  /Users/kevinzhang/Documents/CS/32/NachenBlaster/NachenBlaster/StudentWorld.h \
-  /Users/kevinzhang/Documents/CS/32/NachenBlaster/NachenBlaster/GameWorld.h
--- NachenBlaster/DerivedData/NachenBlaster/Build/Intermediates.noindex/NachenBlaster.build/Debug/NachenBlaster.build/Objects-normal/x86_64/main.d
+++
@@ -1,8 +0,0 @@
-dependencies: \
-  /Users/kevinzhang/Documents/CS/32/NachenBlaster/NachenBlaster/main.cpp \
-  /Users/kevinzhang/Documents/CS/32/NachenBlaster/NachenBlaster/GameController.h \
-  /Users/kevinzhang/Documents/CS/32/NachenBlaster/NachenBlaster/SpriteManager.h \
-  /usr/local/Cellar/freeglut/3.0.0/include/GL/freeglut.h \
-  /usr/local/Cellar/freeglut/3.0.0/include/GL/freeglut_std.h \
-  /usr/local/Cellar/freeglut/3.0.0/include/GL/freeglut_ext.h \
-  /Users/kevinzhang/Documents/CS/32/NachenBlaster/NachenBlaster/GameConstants.h
